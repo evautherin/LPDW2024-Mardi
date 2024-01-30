012d6e6c014f6188f509ba382cf7940a67ed1996
Etienne Vautherin <etienne@vautherin.com>
2024-01-30 11:26:35 +0100

    Initial Commit

---
+++ Mardi/MardiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MardiApp.swift
+//  Mardi
+//
+//  Created by Etienne Vautherin on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MardiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
