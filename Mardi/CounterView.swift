47b908010728b41660101d0000f0869f3472eb5c
Etienne Vautherin <etienne@vautherin.com>
2024-01-31 10:59:01 +0100

    First commit

---
+++ Mardi/CounterView.swift
@@ -0,0 +1,18 @@
+//
+//  CounterView.swift
+//  Mardi
+//
+//  Created by Etienne Vautherin on 30/01/2024.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CounterView()
+}
---
+++ Mardi/CursorView.swift
@@ -0,0 +1,18 @@
+//
+//  CursorView.swift
+//  Mardi
+//
+//  Created by Etienne Vautherin on 31/01/2024.
+//
+
+import SwiftUI
+
+struct CursorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CursorView()
+}
